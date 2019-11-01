c34c48c1bd90d5540ec1bbea8160256f6775efda
Frederik Proske <fproske@gams.com>
2019-11-01 11:48:08 +0100

    allocate error messages to mainWindow

--- components/install-r.js
+++ components/install-r.js
@@ -8,12 +8,12 @@ const fs = require('fs');
 
 async function installRPackages(rpath, apppath, libpath, mainWindow){
   if ( !rpath ) {
-    dialog.showMessageBoxSync({type: 'error',
+    dialog.showMessageBoxSync(mainWindow, {type: 'error',
         title: 'R not found',
         message: 'No R installation was found on your machine. Please install R or specify path manually in settings.'})
     return;
   }
-  const selection = dialog.showMessageBoxSync({
+  const selection = dialog.showMessageBoxSync(mainWindow, {
     type: 'question',
     title: 'Install R packages',
     message: 'Would you like to install the required R packages now?\n\nNote that before doing so, you have to install the system libraries V8 and libcurl.\nOn Debian / Ubuntu you need libcurl4-gnutls-dev and either libv8-dev or libnode-dev, on Fedora use libcurl-devel and v8-devel.',
