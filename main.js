bd07f322882464ce4e49ac8a8117c53fbed75462
Frederik Proske <fproske@gams.com>
2019-11-11 10:32:58 +0100

    updated MIRO and rhandsontable versions

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const log = require('electron-log');
 const menu = require('./components/menu.js');
 const installRPackages = require('./components/install-r.js');
 const requiredAPIVersion = 1;
-const miroVersion = '0.9.0';
+const miroVersion = '0.9.1';
 const exampleAppsData = [
   {
     id: 'pickstock',
