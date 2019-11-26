ccfcd33e7c4d5adb9a84ff165bd969cf5d89a230
Frederik Proske <fproske@gams.com>
2019-11-27 00:35:22 +0100

    jenkins test

--- build/scripts/notarize.js
+++ build/scripts/notarize.js
@@ -1,67 +1,21 @@
 require('dotenv').config();
 const execa = require('execa');
 const path = require('path');
-const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
-  if (electronPlatformName !== 'darwin' || process.env.NO_NOTARIZATION === 'true' ) {
+  if ( electronPlatformName !== 'darwin' || !process.env.CODESIGN_IDENTITY ) {
     return;
   }
-
-  const appName = context.packager.appInfo.productFilename;
-
-  const codesignIdentity = process.env.CODESIGN_IDENTITY;
-
-  if ( !codesignIdentity ) {
-      console.log('Skipping codesign as CODESIGN_IDENTITY was not specified!');
-      return;
+  const appFile = path.join(context.appOutDir, `${context.packager.appInfo.productFilename}.app`);
+  try{
+    const notarizeProc = execa(path.join('.', 'build', 'scripts', 'notarize.sh'), 
+      [appFile], {shell: true});
+    notarizeProc.stderr.pipe(process.stderr);
+    notarizeProc.stdout.pipe(process.stderr);
+    await notarizeProc;
+  } catch (e) {
+      console.log(`Problems notarizing app. Error message: ${e.message}`);
+      throw e;
   }
-  
-  const entitlementsFile = path.join(context.packager.info['_buildResourcesDir'], 
-      'entitlements.mac.plist');
-  const appFile = path.join(context.appOutDir, `${appName}.app`);
-  const frameworksDir = path.join(appFile, 'Contents', 'Frameworks');
-
-  const objectsToSign = [
-  {
-      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libEGL.dylib'),
-      flags: []
-  },
-  {
-      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libffmpeg.dylib'),
-      flags: []
-  },
-  {
-      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libGLESv2.dylib'),
-      flags: []
-  },
-  {
-      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libswiftshader_libEGL.dylib'),
-      flags: []
-  },
-  {
-      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libswiftshader_libGLESv2.dylib'),
-      flags: []
-  },
-  {
-      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Resources', 'crashpad_handler'),
-      flags: ['--options', 'runtime', '--entitlements', entitlementsFile]
-  },
-  {
-      file: path.join(frameworksDir, 'Squirrel.framework', 'Versions', 'A', 'Resources', 'ShipIt'),
-      flags: ['--options', 'runtime', '--entitlements', entitlementsFile]
-  },
-  {
-      file: appFile,
-      flags: ['--options', 'runtime', '--entitlements', entitlementsFile, '--deep']
-  }];
-
-  for ( const objectToSign of objectsToSign ) {
-      const signProc = execa('codesign', ['--sign', codesignIdentity, '--force', 
-         '--timestamp'].concat(objectToSign.flags, objectToSign.file));
-      signProc.stderr.pipe(process.stderr);
-      signProc.stdout.pipe(process.stderr);
-      await signProc;
-  }
+  return;
 };
