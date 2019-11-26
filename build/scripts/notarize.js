cc6dc871a7cc905a70ccde9129e216abf30e4efb
Frederik Proske <fproske@gams.com>
2019-11-26 20:17:03 +0100

    jenkins test

--- build/scripts/notarize.js
+++ build/scripts/notarize.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const execa = require('execa');
+const path = require('path');
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
@@ -7,11 +9,61 @@ exports.default = async function notarizing(context) {
     return;
   }
 
-  if ( !process.env.CODESIGN_IDENTITY ) {
+  const appName = context.packager.appInfo.productFilename;
+
+  const codesignIdentity = process.env.CODESIGN_IDENTITY;
+
+  if ( !codesignIdentity ) {
+      console.log('Skipping codesign as CODESIGN_IDENTITY was not specified!');
       return;
   }
+  
+  const entitlementsFile = path.join(context.packager.info['_buildResourcesDir'], 
+      'entitlements.mac.plist');
+  const appFile = path.join(context.appOutDir, `${appName}.app`);
+  const frameworksDir = path.join(appFile, 'Contents', 'Frameworks');
 
-  const appName = context.packager.appInfo.productFilename;
+  const objectsToSign = [
+  {
+      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libEGL.dylib'),
+      flags: []
+  },
+  {
+      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libffmpeg.dylib'),
+      flags: []
+  },
+  {
+      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libGLESv2.dylib'),
+      flags: []
+  },
+  {
+      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libswiftshader_libEGL.dylib'),
+      flags: []
+  },
+  {
+      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Libraries', 'libswiftshader_libGLESv2.dylib'),
+      flags: []
+  },
+  {
+      file: path.join(frameworksDir, 'Electron Framework.framework', 'Versions', 'A', 'Resources', 'crashpad_handler'),
+      flags: ['--options', 'runtime', '--entitlements', entitlementsFile]
+  },
+  {
+      file: path.join(frameworksDir, 'Squirrel.framework', 'Versions', 'A', 'Resources', 'ShipIt'),
+      flags: ['--options', 'runtime', '--entitlements', entitlementsFile]
+  },
+  {
+      file: appFile,
+      flags: ['--options', 'runtime', '--entitlements', entitlementsFile, '--deep']
+  }];
+
+  for ( const objectToSign of objectsToSign ) {
+      const signProc = execa('codesign', ['--sign', codesignIdentity, '--force', 
+         '--timestamp'].concat(objectToSign.flags, objectToSign.file));
+      signProc.stderr.pipe(process.stderr);
+      signProc.stdout.pipe(process.stderr);
+      await signProc;
+  }        
 
   return await notarize({
     appBundleId: 'com.gams.miro',
