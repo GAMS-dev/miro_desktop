e84006c97fb8c79d90e3c7516a8e6a169dc031fc
Frederik Proske <fproske@gams.com>
2019-12-18 00:43:59 +0100

    test jenkins

--- test/run-tests.js
+++ test/run-tests.js
@@ -8,14 +8,14 @@ const execa = require('execa');
         if ( process.platform === 'win32' ) {
             rPath = path.join('.', 'r', 'bin', 'Rscript');
         }
-        const subproc =  execa(rPath, [ 'run_tests.R' ],
-            { env: { 'LIB_PATH': path.join('.', 'r', 'library')},
-            cwd: path.join('..', 'miro')});
+        console.error(__dirname)
+        const subproc =  execa(rPath, [ path.join(__dirname, '..', 'miro', 'run_tests.R') ],
+            { env: { 'LIB_PATH': path.join(__dirname, '..', 'r', 'library')},
+            cwd: path.join(__dirname, '..', 'miro')});
         subproc.stderr.pipe(process.stderr);
         subproc.stdout.pipe(process.stderr);
         await subproc;
     } catch (e) {
         console.log(`Problems running R tests. Error message: ${e.message}`);
-        exit(1);
     }
 })();
