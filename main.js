87eff57e2e3975011cf2e33dcec270ad90bbb71f
Frederik Proske <fproske@gams.com>
2020-01-16 15:15:01 +0100

    updated miro version

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const log = require('electron-log');
 const menu = require('./components/menu.js');
 const installRPackages = require('./components/install-r.js');
 const requiredAPIVersion = 1;
-const miroVersion = '0.9.37';
+const miroVersion = '0.9.39';
 const libVersion = '1.0';
 const exampleAppsData = [
   {
