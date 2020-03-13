91378b2b90e03237be6d4c7983022f8a2802b43c
Frederik Proske <fproske@gams.com>
2020-03-13 12:01:29 +0100

    updated miro version and publish unstable docker images as well

--- build/scripts/docker-publish.js
+++ build/scripts/docker-publish.js
@@ -2,22 +2,33 @@ const path  = require('path');
 const execa = require('execa');
 const fs = require('fs');
 
+const clArgs = process.argv.slice(2);
+
+let unstableImages = false;
+if ( clArgs[0] === '--unstable' ) {
+    unstableImages = true;
+}
 
 (async () => {
     if ( process.platform != 'linux' ) {
         console.error('Please publish images on Linux!');
         process.exit(1);
     }
-    const miroVersion = fs.readFileSync('version', 'utf8').trim();
-    try {
-        const subproc =  execa('docker', [ 'tag', 'gamsmiro-ui', 'hub.gams.com/gamsmiro-ui' ]);
-        subproc.stderr.pipe(process.stderr);
-        subproc.stdout.pipe(process.stderr);
-        await subproc;
-    } catch (e) {
-        console.log(`Problems tagging docker image. Error message: ${e.message}`);
-        process.exit(1);
+    let miroVersion = 'unstable';
+
+    if ( !unstableImages ) {
+        miroVersion = fs.readFileSync('version', 'utf8').trim();
+        try {
+            const subproc =  execa('docker', [ 'tag', 'gamsmiro-ui', 'hub.gams.com/gamsmiro-ui' ]);
+            subproc.stderr.pipe(process.stderr);
+            subproc.stdout.pipe(process.stderr);
+            await subproc;
+        } catch (e) {
+            console.log(`Problems tagging docker image. Error message: ${e.message}`);
+            process.exit(1);
+        }
     }
+    
     try {
         const subproc =  execa('docker', [ 'tag', 'gamsmiro-ui', `hub.gams.com/gamsmiro-ui:${miroVersion}` ]);
         subproc.stderr.pipe(process.stderr);
@@ -27,15 +38,19 @@ const fs = require('fs');
         console.log(`Problems tagging docker image. Error message: ${e.message}`);
         process.exit(1);
     }
-    try {
-        const subproc =  execa('docker', [ 'push', 'hub.gams.com/gamsmiro-ui' ]);
-        subproc.stderr.pipe(process.stderr);
-        subproc.stdout.pipe(process.stderr);
-        await subproc;
-    } catch (e) {
-        console.log(`Problems pushing docker image. Error message: ${e.message}`);
-        process.exit(1);
+
+    if ( !unstableImages ) {
+        try {
+            const subproc =  execa('docker', [ 'push', 'hub.gams.com/gamsmiro-ui' ]);
+            subproc.stderr.pipe(process.stderr);
+            subproc.stdout.pipe(process.stderr);
+            await subproc;
+        } catch (e) {
+            console.log(`Problems pushing docker image. Error message: ${e.message}`);
+            process.exit(1);
+        }
     }
+    
     try {
         const subproc =  execa('docker', [ 'push', `hub.gams.com/gamsmiro-ui:${miroVersion}` ]);
         subproc.stderr.pipe(process.stderr);
