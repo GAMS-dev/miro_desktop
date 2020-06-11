594161574bc4d79cf8b49fe97d4e86cb8b5645a1
Frederik Proske <fproske@gams.com>
2020-06-11 18:06:50 +0200

    Fix bug in script that writes out third-party licenses

--- update-licenses.sh
+++ update-licenses.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LICENSE_FILE=LICENSE_Launcher.txt
-npm-license-crawler --exclude dist --exclude miro --exclude r-src --production --csv licenses.csv
+npm-license-crawler --exclude dist --exclude r --exclude miro --exclude r-src --production --csv licenses.csv
 cat >$LICENSE_FILE <<EOL
 GAMS MIRO Launcher as a whole is distributed under GPL-3 (GNU GENERAL PUBLIC LICENSE version 3). A copy of this license is included below.
 
@@ -12,10 +12,12 @@ is a list of these components:
 
 EOL
 cat licenses.csv>>$LICENSE_FILE
-cat >$LICENSE_FILE <<EOL
+cat >>$LICENSE_FILE <<EOL
 
 
 EOL
 cat GPL-3.txt>>$LICENSE_FILE
+
+awk -F "\"*,\"*" '{print $2}' licenses.csv | sort |uniq
 \rm -f licenses.csv
 
